d161e8de38b58d71ac84f674f7b24109ef66de91
Legit <pedroclash3245@gmail.com>
2024-09-24 20:43:06 -0300

    primeiro commit

---
+++ app/home/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { colors } from "../../constants/Colors";
+
+export default function Home() {
+  return (
+    <View>
+      <Text>HOME</Text>
+    </View>
+  );
+}
